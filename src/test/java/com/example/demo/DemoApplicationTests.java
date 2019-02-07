98d0a8580b857de2852a1a3524e1178e3516c8c5
Daniel Allen <allen.daniel.r@gmail.com>
2019-02-07 13:45:39 -0800

    Add test case to repro leaks when using reactor-netty based WebClient with timeout

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.netty.http.client.HttpClient;
+import org.springframework.web.reactive.function.client.WebClient;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @RunWith(SpringRunner.class)
@@ -25,7 +26,7 @@ public class DemoApplicationTests {
 	private static final String TEST_CONTENT = "Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content";
 
 	@Test
-	public void reproBufferLeakOnTimeout() {
+	public void noReproBufferLeakOnTimeout() {
 		stubFor(get("/testEndpoint/").willReturn(
 					aResponse()
 						.withStatus(200)
@@ -40,6 +41,35 @@ public class DemoApplicationTests {
 												.responseContent()
 												.aggregate()
 												.asString()
+												.timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
+												.block();
+				System.out.println("HERE: " + responseContent);
+			} catch (RuntimeException re) {
+				System.out.println("HERE: timeout exception: " + re.toString());
+			}
+		}
+		System.gc();
+		for(int i = 0; i < 100000; ++i) {
+			int[] arr = new int[100000];
+		}
+		System.gc();
+	}
+
+	@Test
+	public void reproBufferLeakOnTimeout() {
+		stubFor(get("/testEndpoint/").willReturn(
+					aResponse()
+						.withStatus(200)
+						.withBody(TEST_CONTENT)
+						.withChunkedDribbleDelay(TEST_CONTENT.length()/10, 40)));
+
+		for(int i = 0; i < 20; ++i) {
+			try {
+				String responseContent = WebClient.create()
+												.get()
+												.uri("http://localhost:9005/testEndpoint/")
+												.retrieve()
+												.bodyToMono(String.class)
 												.timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
 												.block();
 				System.out.println("HERE: " + responseContent);
