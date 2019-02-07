5973df9830149616c67146bb6d2b7c7e369406f4
Daniel Allen <allen.daniel.r@gmail.com>
2019-02-07 13:53:03 -0800

    Cleanup

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -16,72 +16,72 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(9005);
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(9005);
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	private static final String TEST_CONTENT = "Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content";
+    private static final String TEST_CONTENT = "Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content Test body content";
 
-	@Test
-	public void noReproBufferLeakOnTimeout() {
-		stubFor(get("/testEndpoint/").willReturn(
-					aResponse()
-						.withStatus(200)
-						.withBody(TEST_CONTENT)
-						.withChunkedDribbleDelay(TEST_CONTENT.length()/10, 40)));
+    @Test
+    public void noReproBufferLeakOnTimeout() {
+        stubFor(get("/testEndpoint/").willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody(TEST_CONTENT)
+                        .withChunkedDribbleDelay(TEST_CONTENT.length()/10, 40)));
 
-		for(int i = 0; i < 20; ++i) {
-			try {
-				String responseContent = HttpClient.create()
-												.get()
-												.uri("http://localhost:9005/testEndpoint/")
-												.responseContent()
-												.aggregate()
-												.asString()
-												.timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
-												.block();
-				System.out.println("HERE: " + responseContent);
-			} catch (RuntimeException re) {
-				System.out.println("HERE: timeout exception: " + re.toString());
-			}
-		}
-		System.gc();
-		for(int i = 0; i < 100000; ++i) {
-			int[] arr = new int[100000];
-		}
-		System.gc();
-	}
+        for(int i = 0; i < 20; ++i) {
+            try {
+                String responseContent = HttpClient.create()
+                                                .get()
+                                                .uri("http://localhost:9005/testEndpoint/")
+                                                .responseContent()
+                                                .aggregate()
+                                                .asString()
+                                                .timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
+                                                .block();
+                System.out.println("HERE: " + responseContent);
+            } catch (RuntimeException re) {
+                System.out.println("HERE: timeout exception: " + re.toString());
+            }
+        }
+        System.gc();
+        for(int i = 0; i < 100000; ++i) {
+            int[] arr = new int[100000];
+        }
+        System.gc();
+    }
 
-	@Test
-	public void reproBufferLeakOnTimeout() {
-		stubFor(get("/testEndpoint/").willReturn(
-					aResponse()
-						.withStatus(200)
-						.withBody(TEST_CONTENT)
-						.withChunkedDribbleDelay(TEST_CONTENT.length()/10, 40)));
+    @Test
+    public void reproBufferLeakOnTimeout() {
+        stubFor(get("/testEndpoint/").willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody(TEST_CONTENT)
+                        .withChunkedDribbleDelay(TEST_CONTENT.length()/10, 40)));
 
-		for(int i = 0; i < 20; ++i) {
-			try {
-				String responseContent = WebClient.create()
-												.get()
-												.uri("http://localhost:9005/testEndpoint/")
-												.retrieve()
-												.bodyToMono(String.class)
-												.timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
-												.block();
-				System.out.println("HERE: " + responseContent);
-			} catch (RuntimeException re) {
-				System.out.println("HERE: timeout exception: " + re.toString());
-			}
-		}
-		System.gc();
-		for(int i = 0; i < 100000; ++i) {
-			int[] arr = new int[100000];
-		}
-		System.gc();
-	}
+        for(int i = 0; i < 20; ++i) {
+            try {
+                String responseContent = WebClient.create()
+                                                .get()
+                                                .uri("http://localhost:9005/testEndpoint/")
+                                                .retrieve()
+                                                .bodyToMono(String.class)
+                                                .timeout(Duration.ofMillis(ThreadLocalRandom.current().nextInt(1, 35)))
+                                                .block();
+                System.out.println("HERE: " + responseContent);
+            } catch (RuntimeException re) {
+                System.out.println("HERE: timeout exception: " + re.toString());
+            }
+        }
+        System.gc();
+        for(int i = 0; i < 100000; ++i) {
+            int[] arr = new int[100000];
+        }
+        System.gc();
+    }
 
 }
